78399ea7a4b896ccfad7d7455fa5b474d16acb66
raem-lpr <kostya_0710s@mail.ru>
2022-11-02 23:33:25 +0300

    Lab8 Release

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# infa_2022_raem_lpr
+## infa_2022_raem_lpr
+
+#### :white_check_mark: [lab8](https://github.com/RAEM-LPR/infa_2022_raem_lpr/blob/main/lab8/game.py)
--- lab8/game.py
+++ lab8/game.py
@@ -1,15 +1,20 @@
-from curses.textpad import rectangle
-from pickle import FALSE
 import pygame
 from pygame.draw import *
 from random import randint
 pygame.init()
 
-FPS = 1
-screen = pygame.display.set_mode((1200, 900))
+
+MAX_TARGETS_NUMBER = 3 # max number of targets on screen
+FPS = 30
+BALLS_DELAY = 30  #create new target each 30 frame 
+BALLS_DELAY_COUNTER = 0
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 900
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.font.init()
 my_font = pygame.font.SysFont('Comic Sans MS', 30)
-
+SCORE_TEXT_POSITION = (0,0)
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -21,78 +26,117 @@ BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 
-class gamescore(object):
-    """docstring"""
- 
-    def __init__(self, maxtarget=1):
-        """Constructor"""
-        self.score = 0
-        self.total = 0
-        self.maxtarget = maxtarget
-        self.targets=[]
-        
-    def increm(self):
-        self.score += 1
+class Target:
+    """Target class"""
     
-    def next(self):
-        if(len(self.targets)>=self.maxtarget):
-            for i in range(0, self.maxtarget-1):
-                self.targets[i]=self.targets[i+1]            
-            self.targets[self.maxtarget-1] = target()  
-        else:
-            self.targets.append(target())
-        self.total += 1
-        
-    def scorestr(self):
-        return ('Score: ' + str(score) + '/' + str(total))
-    
-    
-
-
-class target(object):
-    """docstring"""
- 
     def __init__(self):
         """Constructor"""
         self.color = COLORS[randint(0, 5)]
-        self.x = randint(100,700)
-        self.y = randint(100,500)
+        self.x = randint(100,SCREEN_WIDTH-100)
+        self.y = randint(100,SCREEN_HEIGHT-100)
         self.r = randint(30,50)
+        self.moveVector = [randint(-5,5), randint(-5,5)]
         if randint(0,10) == 0: 
-            self.isSimple = False
+            self.isSimple = False # shape: square, motion: random
+            self.score = 3
         else:
-            self.isSimple = True
+            self.isSimple = True # shape: circle, motion: linear 
+            self.score = 1
     
     def draw(self):
-        if(self.isSimple):
+        """draw this target on screen"""
+        if self.isSimple:
             circle(screen, self.color, (self.x, self.y), self.r)
         else:          
             polygon(screen, self.color, [(self.x,self.y), (self.x+self.r,self.y),
                                          (self.x+self.r,self.y+self.r), (self.x,self.y+self.r)])
-    def clickchek(self, event):
-        if(self.isSimple):
-            
-            return True
+    def clickcheck(self, event):
+        """
+        checking that the target is hit
+        event - your mouse click
+        """
+        if self.isSimple:
+            if( ((self.x-event.pos[0])**2 + (self.y-event.pos[1])**2) <= self.r**2):
+                return True
+            else:
+                return False
+        else:
+            if( (self.x <= event.pos[0]) and (self.x + self.r >= event.pos[0]) and (self.y <= event.pos[1]) and (self.y+self.r >= event.pos[1]) ):
+                return True
+            else:
+                return False
+                
+    def move(self):
+        """this target move to one step"""
+        if not self.isSimple: # new motion direction for square target
+            if abs(self.moveVector[0]) > 10:
+              self.moveVector[0] = 0
+            if abs(self.moveVector[1]) > 10:
+              self.moveVector[1] = 0
+            self.moveVector[0] +=randint(-2,2) 
+            self.moveVector[1] +=randint(-2,2)            
+        self.x += self.moveVector[0]
+        self.y += self.moveVector[1]
+        if self.x <= self.r or self.x >= gscore.screen_width - self.r:
+            self.moveVector[0] = -self.moveVector[0]
+        if self.y <= self.r or self.y >= gscore.screen_height - self.r:
+            self.moveVector[1] = -self.moveVector[1]
+
+class Game:
+    """main game class"""
+ 
+    def __init__(self, SCREEN_SIZE, maxtarget=1):
+        """
+        Constructor
+        maxtarget - max number of targets on screen
+        SCREEN_SIZE - game screen size
+        """
+        self.score = 0 # your game score
+        self.total = 0 # total targets generated
+        self.maxtarget = maxtarget
+        self.targets = []
+        self.screen_height = SCREEN_SIZE[1]
+        self.screen_width = SCREEN_SIZE[0]
+    
+    def next(self):
+        """generate new target"""
+        if(len(self.targets)>=self.maxtarget):
+            for i in range(0, self.maxtarget-1):
+                self.targets[i]=self.targets[i+1]            
+            self.targets[self.maxtarget-1] = Target()  
+        else:
+            self.targets.append(Target())
+        self.total += 1
         
-        return False
+    def scorestr(self):
+        """return your score string to display on screen"""
+        return ('Score: ' + str(self.score) + '/Targets: ' + str(self.total))
+        
+    def checkclick(self, event):
+        """
+        checking that any target is hit
+        event - your mouse click
+        """
+        for i in range(len(self.targets)):
+            if self.targets[i]:
+                if self.targets[i].clickcheck(event):
+                    self.score += self.targets[i].score
+                    self.targets[i] = None
+    def draw(self):
+        """draw all active targets on screen"""
+        screen.fill(BLACK)
+        screen.blit(my_font.render( gscore.scorestr(), False, GREEN) , SCORE_TEXT_POSITION)        
+        for t in self.targets:
+            if t:
+                t.draw()
+                
+    def move(self):
+        """all active targets move to one step"""
+        for t in self.targets:
+            if t:
+                t.move()
     
-
-gscore = gamescore(2)
-
-def new_ball():
-    '''рисует новый шарик '''
-    global x, y, r, total
-    x = randint(100,700)
-    y = randint(100,500)
-    r = randint(30,50)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
-    total = total + 1
-    
-def click(event):
-    global score,x,y,r
-    if( ((x-event.pos[0])**2+(y-event.pos[1])**2) <= r**2):
-        score = score + 1
+gscore = Game(SCREEN_SIZE, MAX_TARGETS_NUMBER)
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -100,14 +144,18 @@ finished = False
 
 while not finished:
     clock.tick(FPS)
-    screen.blit(my_font.render(('Score: ' + str(score) + '/' + str(total)), False, GREEN) , (0,0))
+    BALLS_DELAY_COUNTER += 1    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            click(event)
-    new_ball()
+            gscore.checkclick(event)
+    gscore.move()
+    if BALLS_DELAY_COUNTER == BALLS_DELAY:
+        BALLS_DELAY_COUNTER = 0
+        gscore.next()       
+    gscore.draw()
     pygame.display.update()
-    screen.fill(BLACK)  
+    
 
 pygame.quit()
